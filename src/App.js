5e7205dc92f7d6156716267bf27370f79959b611
Edgardo Romero Aguero <eromeroaguero@gmail.com>
2019-11-05 13:02:55 -0500

    added redux, and implemented userReducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,49 +1,59 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import {auth} from './firebase/firebase.util';
-import CollectionPreview from './components/collection-preview/collection-preview.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.util';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+    const {setCurrentUser} = this.props;
 
-      console.log(user);
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+           setCurrentUser({
+              id: snapShot.id,
+              ...snapShot.data()
+            });
+          });
+        }
+
+      setCurrentUser(userAuth);
+    });
   }
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
-  
+
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
-        <Switch> 
+        <Header />
+        <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
       </div>
     );
-    }  
+  }
 }
 
-export default App;
 
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -3,23 +3,47 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyBabJ0G9A8oi8Rv7zAiyTBtpmeD0069tyk",
-    authDomain: "crwn-db-e23ad.firebaseapp.com",
-    databaseURL: "https://crwn-db-e23ad.firebaseio.com",
-    projectId: "crwn-db-e23ad",
-    storageBucket: "crwn-db-e23ad.appspot.com",
-    messagingSenderId: "369218744725",
-    appId: "1:369218744725:web:647d7ed75695c094e3069c"
-  };
+  apiKey: "AIzaSyBabJ0G9A8oi8Rv7zAiyTBtpmeD0069tyk",
+  authDomain: "crwn-db-e23ad.firebaseapp.com",
+  databaseURL: "https://crwn-db-e23ad.firebaseio.com",
+  projectId: "crwn-db-e23ad",
+  storageBucket: "crwn-db-e23ad.appspot.com",
+  messagingSenderId: "369218744725",
+  appId: "1:369218744725:web:647d7ed75695c094e3069c"
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
 
-const  provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({prompt: 'select_account'});
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
-
